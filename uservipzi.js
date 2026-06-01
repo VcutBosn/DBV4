6d0d53ecadc30ba108c5c434f6afdde320c07880
VcutBosn <yayagg71@gmail.com>
2026-06-01 12:24:29 +0800

    Update uservipzi.js

--- uservipzi.js
+++ uservipzi.js
@@ -3,14 +3,6 @@
         {
             "email": "admin",
             "password": "admin123"
-        },
-        {
-            "email": "vipzi",
-            "password": "vipzi"
-        },
-        {
-            "email": "toya",
-            "password": "toya123"
         }
     ]
 }

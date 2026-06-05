e0042f9f31561ffbe90c564f5d2443b6a3f9aeb8
VcutBosn <yayagg71@gmail.com>
2026-06-05 08:20:20 +0800

    Update uservipzi.js

--- uservipzi.js
+++ uservipzi.js
@@ -1,9 +1,28 @@
 {
-  "users": [
-    {
-      "username": "admin",
-      "password": "admin123"
-    }
-  ],
-  "lastUpdated": "2026-01-01T00:00:00.000Z"
+    "users": [
+        {
+            "id": 1,
+            "username": "vipzix",
+            "password": "master123",
+            "level": "admim",
+            "expired": "2027-12-31",
+            "created_at": "2026-01-01"
+        },
+        {
+            "id": 2,
+            "username": "user2",
+            "password": "pass456",
+            "level": "user",
+            "expired": "2026-08-15",
+            "created_at": "2026-01-15"
+        },
+        {
+            "id": 3,
+            "username": "trial",
+            "password": "trial123",
+            "level": "trial",
+            "expired": "2026-06-30",
+            "created_at": "2026-06-01"
+        }
+    ]
 }

77ed64131be84cddbb4bd784542243e54ad83410
VcutBosn <yayagg71@gmail.com>
2026-06-05 08:57:42 +0800

    Update and rename uservipzi.js to users.js

---
+++ users.js
@@ -0,0 +1,36 @@
+{
+    "users": [
+        {
+            "id": 1,
+            "username": "owner",
+            "password": "owner123",
+            "level": "owner",
+            "expired": "2099-12-31",
+            "created_at": "2026-01-01"
+        },
+        {
+            "id": 2,
+            "username": "admin",
+            "password": "admin123",
+            "level": "admin",
+            "expired": "2028-12-31",
+            "created_at": "2026-01-01"
+        },
+        {
+            "id": 3,
+            "username": "vip",
+            "password": "vip123",
+            "level": "vip",
+            "expired": "2027-12-31",
+            "created_at": "2026-01-15"
+        },
+        {
+            "id": 4,
+            "username": "user",
+            "password": "user123",
+            "level": "user",
+            "expired": "2026-12-31",
+            "created_at": "2026-02-01"
+        }
+    ]
+}
